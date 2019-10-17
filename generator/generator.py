34e2d6fd78ed37799be817895557663f5eef6c51
Albert <9albert6@github.com>
2019-10-17 17:54:21 +0200

    first attemptions of connectivity beetwen services

--- generator/generator.py
+++ generator/generator.py
@@ -3,20 +3,21 @@ import pika
 import time
 
 import socket
-
 pingcounter = 0
 isreachable = False
 while isreachable is False and pingcounter < 5:
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect(('rabbitmq', 5672))
         isreachable = True
     except socket.error as e:
-        time.sleep(2)
+        time.sleep(10)
         pingcounter += 1
     s.close()
 
 if isreachable:
+    print("Starting generating numbers")
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
--- ingest/ingest.py
+++ ingest/ingest.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python3
 import pika
 import time
+from typing import List, Dict
+import mysql.connector
 
 import socket
 
 pingcounter = 0
-isreachable = False
-while isreachable is False and pingcounter < 5:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect(('rabbitmq', 5672))
-        isreachable = True
-    except socket.error as e:
-        time.sleep(2)
-        pingcounter += 1
-    s.close()
+# isreachable = False
+# while isreachable is False and pingcounter < 5:
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     try:
+#         s.connect(('rabbitmq', 5672))
+#         isreachable = True
+#     except socket.error as e:
+#         time.sleep(2)
+#         pingcounter += 1
+#     s.close()
 
-if isreachable:
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='rabbitmq'))
-    channel = connection.channel()
+# if isreachable:
+print("Starting generating numbers")
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
-
-
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+channel.queue_declare(queue='hello')
 
 
-    channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'database',
+        'port': '3306',
+        'database': 'fibbonaci'
+    }   
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    val = ("3", "red")
+    cursor.execute('INSERT INTO fibbonaci_numbers (number_id, title) VALUES ("3", "red")')
+    cursor.close()
+    connection.commit()
+    connection.close()
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+channel.basic_consume(
+    queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
